9c64b93f37205e914744f4528688b5f421bf65be
unclejamal <unclejamal@gmail.com>
2020-01-20 20:42:08 +0000

    Refactor - simplify unit test

--- src/test/java/shop/PriceBasketCommandTest.java
+++ src/test/java/shop/PriceBasketCommandTest.java
@@ -1,35 +1,19 @@
 package shop;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 import shop.ui.CommandLineOutput;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static java.util.Arrays.asList;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class PriceBasketCommandTest {
-    private Basket basket = Mockito.mock(Basket.class);
-    private CommandLineOutput commandLineOutput = Mockito.mock(CommandLineOutput.class);
+    private Basket basket = mock(Basket.class);
+    private CommandLineOutput commandLineOutput = mock(CommandLineOutput.class);
     private PriceBasketCommand command = new PriceBasketCommand(commandLineOutput);
 
-    @Test
-    public void emptyBasket() {
-        when(basket.getBasketPricing()).thenReturn(BasketPricing.EMPTY_BASKET_PRICING);
-        command.handlePriceBasket(basket);
-        verify(commandLineOutput).showBasketPricing(BasketPricing.EMPTY_BASKET_PRICING);
-    }
 
     @Test
-    public void nonEmptyBasket() {
-        Set<BasketPosition> basketContent = new HashSet<>(asList(
-                new BasketPosition(1, new ProductMetadata("apple", "apples", 0.10d)),
-                new BasketPosition(3, new ProductMetadata("car", "cars", 1000.0d))
-        ));
-        BasketPricing basketPricing = new BasketPricing(1.55d, basketContent);
+    public void passesBasketPricingToCommandLineOutput() {
+        BasketPricing basketPricing = mock(BasketPricing.class);
         when(basket.getBasketPricing()).thenReturn(basketPricing);
 
         command.handlePriceBasket(basket);
