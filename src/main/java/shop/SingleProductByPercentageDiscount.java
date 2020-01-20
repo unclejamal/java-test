f38723362862b92a437cfb58a56ab66ae7c5204a
unclejamal <unclejamal@gmail.com>
2020-01-20 22:34:20 +0000

    Add single product by percentage discount

---
+++ src/main/java/shop/SingleProductByPercentageDiscount.java
@@ -0,0 +1,21 @@
+package shop;
+
+public class SingleProductByPercentageDiscount implements Discount {
+
+    private final ProductMetadata discountedProduct;
+    private final int howManyPercentCheaper;
+
+    public SingleProductByPercentageDiscount(ProductMetadata discountedProduct, int howManyPercentCheaper) {
+        this.discountedProduct = discountedProduct;
+        this.howManyPercentCheaper = howManyPercentCheaper;
+    }
+
+    @Override
+    public double applyTo(Basket basket) {
+        return basket.countProduct(discountedProduct) * applyPercentage();
+    }
+
+    private double applyPercentage() {
+        return howManyPercentCheaper * discountedProduct.price / 100;
+    }
+}
---
+++ src/test/java/shop/SingleProductByPercentageDiscountTest.java
@@ -0,0 +1,42 @@
+package shop;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SingleProductByPercentageDiscountTest {
+
+    private final ProductMetadata apple = new ProductMetadata("apple", "apples", 1.00d);
+    private final ProductMetadata orange = new ProductMetadata("orange", "orange", 0.80d);
+    private final Basket basket = new Basket();
+
+    private final SingleProductByPercentageDiscount discount = new SingleProductByPercentageDiscount(apple, 10);
+
+    @Test
+    public void applicableOnce() {
+        basket.addProduct(1, apple);
+        double discountValue = discount.applyTo(basket);
+        assertThat(discountValue, equalTo(0.10d));
+    }
+
+    @Test
+    public void applicableTwice() {
+        basket.addProduct(2, apple);
+        double discountValue = discount.applyTo(basket);
+        assertThat(discountValue, equalTo(0.20d));
+    }
+
+    @Test
+    public void notApplicable_emptyBasket() {
+        double discountValue = discount.applyTo(basket);
+        assertThat(discountValue, equalTo(0.00d));
+    }
+
+    @Test
+    public void notApplicable_productMissingInBasket() {
+        basket.addProduct(1, orange);
+        double discountValue = discount.applyTo(basket);
+        assertThat(discountValue, equalTo(0.00d));
+    }
+}
