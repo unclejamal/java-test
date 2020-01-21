0604e297cd32a62dfb26b9c7f6c6fa7f3d512e67
unclejamal <unclejamal@gmail.com>
2020-01-21 09:53:34 +0000

    Make sure DiscountingProcess works when no discounts exists

--- src/test/java/shop/discount/DiscountingProcessTest.java
+++ src/test/java/shop/discount/DiscountingProcessTest.java
@@ -4,9 +4,20 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import shop.Basket;
 
+import static shop.discount.Discount.NO_DISCOUNT;
+
 public class DiscountingProcessTest {
 
     private final Basket basket = Mockito.mock(Basket.class);
+
+    @Test
+    public void noDiscountsExist() {
+        DiscountingProcess discountingProcess = new DiscountingProcess();
+
+        discountingProcess.applyTo(basket);
+
+        Mockito.verify(basket).addDiscountForValueOf(NO_DISCOUNT);
+    }
 
     @Test
     public void passesDiscountsToBasket() {
